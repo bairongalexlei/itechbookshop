373dd9034e051ec68e7e687fd250fb58ec8aa0a7
AlexLei <bairongalexlei@gmail.com>
2015-05-13 22:00:01 -0400

    Account form half done

---
+++ BookShop/Common/CommonEnum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Common
+{
+    public class CommonEnum
+    {
+        #region Receipt Type
+        public enum ReceiptType
+        {
+            [DisplayValue("Individual")]
+            Individual = 1,
+            [DisplayValue("Organization")]
+            Organization = 2,
+            [DisplayValue("Others")]
+            Others = 3
+        }
+        #endregion
+
+        #region Status
+        public enum Status
+        {
+            [DisplayValue("Active")]
+            Active = 1,
+            [DisplayValue("Deleted")]
+            Deleted = 2
+        }
+        #endregion
+
+        #region Status
+        public enum UserType
+        {
+            [DisplayValue("CFO")]
+            CFO = 1,
+            [DisplayValue("Other")]
+            Other = 2
+        }
+        #endregion
+    }
+}
---
+++ BookShop/Common/StringEnum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Common
+{
+    class StringEnum
+    {
+
+    }
+
+    #region Class DisplayValueAttribute
+
+    /// <summary>
+    /// Simple attribute class for storing String Values
+    /// </summary>
+    public class DisplayValueAttribute : Attribute
+    {
+        private string _value;
+
+        /// <summary>
+        /// Creates a new <see cref="DisplayValueAttribute"/> instance.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        public DisplayValueAttribute(string value)
+        {
+            _value = value;
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value></value>
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+
+    #endregion
+}
